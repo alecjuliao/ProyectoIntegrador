c9a62285e44f7e0a2fcd13e5997fcb7beee51ec5
alec <ajuliao@udesa.edu.ar>
2019-11-08 11:31:28 -0300

    puse el windows onload y la barra de busqueda

--- api.js
+++ api.js
@@ -1,5 +1,17 @@
+window.onload = function(){
+
 fetch('https://api.themoviedb.org/3/tv/popular?api_key=e7db2f9ff5c491a6d4dbf8f051193e9b&language=en-US&page=1
 ')
   .then(function(response) {
     return response.json();
   })
+
+
+
+
+
+
+
+
+
+}
