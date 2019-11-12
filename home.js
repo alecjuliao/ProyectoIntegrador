3161c509d832ab445f50e5e537cae92d5479a6b5
Franco Mendelsohn <mendelsohn82@gmail.com>
2019-11-12 17:43:02 -0300

    pequenos cambio y JS

---
+++ home.js
@@ -0,0 +1,4 @@
+var bebecito = ["anuel","dobleA"];
+var bebecita = ["karol","g"];
+var bebecitos = [bebecito,bebecita];
+var mensaje = "caramelito me podes dar tu numero asi te rapto?"; prompt(mensaje);
