a8c384a67659e2a243742fb3545a76a87e292347
Franco Mendelsohn <mendelsohn82@gmail.com>
2019-11-12 21:03:03 -0300

    Logo + creacion de carptas

--- home.js
+++
@@ -1,4 +0,0 @@
-var bebecito = ["anuel","dobleA"];
-var bebecita = ["karol","g"];
-var bebecitos = [bebecito,bebecita];
-var mensaje = "caramelito me podes dar tu numero asi te rapto?"; prompt(mensaje);
