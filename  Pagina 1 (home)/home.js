4393e7497f2551346da58d53b39fd99d3920e920
jan <jjuliao@udesa.edu.ar>
2019-11-15 14:07:10 -0300

    todo

---  Pagina 1 (home)/home.js
+++  Pagina 1 (home)/home.js
@@ -12,7 +12,22 @@ window.onload = function (){
 
          }
 
-         
+
+
+
+})
+fetch("https://api.themoviedb.org/3/tv/popular?api_key=c737da8edba65cd6dc516d8db8f7bf5b&language=en-US&page=1")
+  .then(function(response) {
+    return response.json();
+  })
+
+  .then(function(rated) {
+  var seriesRated = document.querySelector ("#populares")
+  for(var i = 0; i < rated.results.length; i++) {
+         console.log(rated.results[i]);
+           seriesRated.innerHTML += '<li>'+ '<a href="#">' + '<img src="https://image.tmdb.org/t/p/w300/' + rated.results[i].poster_path + '">' + '</a>'+ '</li>'
+
+       }
 
 
 
@@ -20,35 +35,23 @@ window.onload = function (){
 })
 
 
+fetch("https://api.themoviedb.org/3/tv/airing_today?api_key=c737da8edba65cd6dc516d8db8f7bf5b&language=en-US&page=1")
+  .then(function(response) {
+    return response.json();
+  })
+
+  .then(function(rated) {
+  var seriesRated = document.querySelector ("#alAire")
+  for(var i = 0; i < rated.results.length; i++) {
+         console.log(rated.results[i]);
+           seriesRated.innerHTML += '<li>'+ '<a href="#">' + '<img src="https://image.tmdb.org/t/p/w300/' + rated.results[i].poster_path + '">' + '</a>'+ '</li>'
+
+       }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})
 
 
 
