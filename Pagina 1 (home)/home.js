320b00df87e8cbb7e8674d684094ed86606d3bba
alecjuliao <56725609+alecjuliao@users.noreply.github.com>
2019-11-21 18:20:30 -0300

    Arregle carousel

--- Pagina 1 (home)/home.js
+++ Pagina 1 (home)/home.js
@@ -1,18 +1,18 @@
 window.onload = function (){
-  fetch("https://api.themoviedb.org/3/tv/top_rated?api_key=c737da8edba65cd6dc516d8db8f7bf5b&language=en-US&page=1"s)
+  fetch("https://api.themoviedb.org/3/tv/top_rated?api_key=c737da8edba65cd6dc516d8db8f7bf5b&language=en-US&page=1")
     .then(function(response) {
       return response.json();
     })
+
     .then(function(rated) {
-    var seriesRated = document.querySelector ("listaMejorPunt")
-for (var i = 0; i < rated.results.lenght; i++)
-seriesRated.innerHTML += '<li>' + '<a href="../info/info.html">' + '<img src="https://image.tmdb.org/t/p/w300/' + rated.results[i].poster_path + '">' + '</a>' + '</li>'
+    var seriesRated = document.querySelector ("#listaMejorPunt")
+    for(var i = 0; i < rated.results.length; i++) {
+             seriesRated.innerHTML += '<li>'+ '<a href="#">' + '<img src="https://image.tmdb.org/t/p/w300/' + rated.results[i].poster_path + '">' + '</a>'+ '</li>'
+
+         }
 
 
-
-
-
-})
+  })
 fetch("https://api.themoviedb.org/3/tv/popular?api_key=c737da8edba65cd6dc516d8db8f7bf5b&language=en-US&page=1")
   .then(function(response) {
     return response.json();
@@ -21,12 +21,9 @@ fetch("https://api.themoviedb.org/3/tv/popular?api_key=c737da8edba65cd6dc516d8db
   .then(function(rated) {
   var seriesRated = document.querySelector ("#populares")
   for(var i = 0; i < rated.results.length; i++) {
-         console.log(rated.results[i]);
            seriesRated.innerHTML += '<li>'+ '<a href="#">' + '<img src="https://image.tmdb.org/t/p/w300/' + rated.results[i].poster_path + '">' + '</a>'+ '</li>'
 
        }
-
-
 
 
 })
